f91472d9bf2e690bd53ce7d4d7873e8ef0ffa9ac
Victor <victor.huanqui@usach.cl>
2019-05-05 22:14:28 -0400

    Hijo funcionando
    Realiza los cálculos pedidos por medio de listas enlazadas. Solo falta devolver la información obtenida :3

--- child.c
+++ child.c
@@ -17,8 +17,10 @@ float getAverageR(visibilityList_s *visibilityList, int len)
     float summR = 0;
     visibilityList_s *auxVisibilityList = visibilityList;
 
-    while(auxVisibilityList->next != NULL)
-        summR = (visibilityList->visibility)->r + summR;
+    while(auxVisibilityList != NULL){
+        summR = (auxVisibilityList->visibility)->r + summR;
+        auxVisibilityList = auxVisibilityList->next;
+    }
 
     return summR/len;
 }   
@@ -29,8 +31,10 @@ float getAverageI(visibilityList_s *visibilityList, int len)
     float summI = 0;
     visibilityList_s *auxVisibilityList = visibilityList;
 
-    while(auxVisibilityList->next != NULL)
-        summI = (visibilityList->visibility)->i + summI;
+    while(auxVisibilityList != NULL){
+        summI = (auxVisibilityList->visibility)->i + summI;
+        auxVisibilityList = auxVisibilityList->next;   
+    }
 
     return summI/len;
 }
@@ -41,8 +45,10 @@ float getPow(visibilityList_s *visibilityList)
     float summW = 0;
     visibilityList_s *auxVisibilityList = visibilityList;
 
-    while(auxVisibilityList->next != NULL)
-        summW = sqrt(pow((visibilityList->visibility)->r, 2) + pow((visibilityList->visibility)->i, 2)) + summW;
+    while(auxVisibilityList != NULL){
+        summW = sqrt(pow((auxVisibilityList->visibility)->r, 2) + pow((auxVisibilityList->visibility)->i, 2)) + summW;
+        auxVisibilityList = auxVisibilityList->next;
+    }
 
     return summW;
 }
@@ -53,14 +59,21 @@ float getNoise(visibilityList_s *visibilityList)
     float summR = 0;
     visibilityList_s *auxVisibilityList = visibilityList;
 
-     while(auxVisibilityList->next != NULL)
-        summR = (visibilityList->visibility)->w + summR;
+     while(auxVisibilityList != NULL){
+        summR = (auxVisibilityList->visibility)->w + summR;
+        auxVisibilityList = auxVisibilityList->next;
+     }
 
     return summR;
 }
 
 visibilityList_s *createList(visibility_s *newVisibility)
 {   
+    if(newVisibility == NULL){
+        perror("Empty struct");
+        exit(-1);
+    }
+
     visibilityList_s *visibilityList =  malloc(sizeof(visibilityList_s));
     visibilityList->visibility = malloc(sizeof(visibility_s));
 
@@ -73,15 +86,28 @@ visibilityList_s *createList(visibility_s *newVisibility)
 
 void appendVisibility(visibilityList_s **visibilityList, visibility_s *newVisibility)
 {
-    visibilityList_s *auxVisibility = *visibilityList;
-    (auxVisibility->next)->visibility = newVisibility;
-    (auxVisibility->next)->next = *visibilityList;
+    visibilityList_s *newVisibilityList =  malloc(sizeof(visibilityList_s));   
+    newVisibilityList->visibility = newVisibility;
+    
+    newVisibilityList->next = (*visibilityList);
+    *visibilityList = newVisibilityList;
+}
+
+void printDataList(visibilityList_s *visibilityList){
+    visibilityList_s *auxVisibilityList = visibilityList;
+    printf("Star data -------------\n");
+    while(auxVisibilityList != NULL){
+        printf("(%f %f %f %f %f)\n", (auxVisibilityList->visibility)->u, (auxVisibilityList->visibility)->v,
+            (auxVisibilityList->visibility)->r, (auxVisibilityList->visibility)->i, (auxVisibilityList->visibility)->w);
+
+        auxVisibilityList = auxVisibilityList->next; 
+    } 
+    printf("End data --------------\n");
 }
 
 
 int main(int argc, char *argv[]) 
 {
-    printf("\n\n******************  CHILD PROGRAM %d  ******************\n\n", getpid());
     //printf("Soy el hijo: %d y mi papi es: %d\n", getpid(), getppid());
     
     visibility_s * visibility = malloc(sizeof(visibility_s));
@@ -92,6 +118,7 @@ int main(int argc, char *argv[])
     float test = distance(visibility);
 
     visibilityList = createList(visibility);
+    printDataList(visibilityList);
     
     //printf("Mi papi me está pasando origin_distance: %f con status: %d y yo soy: %d\n", test, visibility->status, getpid()) ;
     
@@ -99,26 +126,26 @@ int main(int argc, char *argv[])
     {
         if (visibility->status == 1)
         {
-            float test = distance(visibility);
             visibility->status = 0;
-
             appendVisibility(&visibilityList, visibility);
             len = len + 1;
-            //printf("Mi papi me está pasando origin_distance: %f con status: %d y yo soy: %d, en el while\n", test, visibility->status, getpid()) ;
-            printf("(((((%d)))))\n", len);
+            //printf("Mi papi me está pasando origin_distance: %f con status: %d y yo soy: %d, en el while\n", test, visibility->status, getpid());
         }
+        
+        visibility = malloc(sizeof(visibility_s));
         read(0, visibility, sizeof(visibility_s));
-
     }
 
 
-    printf("@@@@@@@@([%d] %f, %f, %f, %f)@@@@@@@@\n", len, getAverageR(visibilityList, len), getAverageI(visibilityList, len),
+
+
+    printf("---([%d] %f, %f, %f, %f)---\n", len, getAverageR(visibilityList, len), getAverageI(visibilityList, len),
         getPow(visibilityList), getNoise(visibilityList));
 
-    printf("Soy el hijo: %d y mi papi es: %d, me mataron\n", getpid(), getppid());
+   //printf("Soy el hijo: %d y mi papi es: %d, me mataron\n", getpid(), getppid());
 
     write(1, visibility, sizeof(visibility_s));
-    printf("\n\n******************  END CHILD %d  ******************\n\n", getpid());
+
 
     return 0;
 }
